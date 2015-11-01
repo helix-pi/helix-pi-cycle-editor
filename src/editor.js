6314916240f1101679493470ce03ae09640c2ea3
Nick Johnstone <ncwjohnstone@gmail.com>
2015-11-01 16:52:19 +1300

    Now I have three actors (lol)

--- src/editor.js
+++ src/editor.js
@@ -1,8 +1,22 @@
 import Rx from 'rx';
 import {h} from '@cycle/dom';
 
+Rx.config.longStackSupport = true;
+
+const {div} = require('hyperscript-helpers')(h);
+
+function editorView () {
+  return (
+    div('.editor', [
+      div('.actor', 'hey'),
+      div('.actor', 'hey'),
+      div('.actor', 'hey')
+    ])
+  );
+}
+
 export default function editor ({DOM}) {
   return {
-    DOM: Rx.Observable.just(h('div', 'hizazzz'))
+    DOM: Rx.Observable.just(editorView())
   };
 }
--- test/editor-test.js
+++ test/editor-test.js
@@ -1,10 +1,32 @@
 /* globals describe, it */
 
-const assert = require('assert');
-const editor = require('../src/editor');
+import assert from 'assert';
+
+import editor from '../src/editor';
+
+import {mockDOMResponse} from '@cycle/dom';
+import $ from 'vdom-query';
 
 describe('the Helix Pi Editor', () => {
   it('exists', () => {
     assert.equal(!!editor, true);
+  });
+
+  it('can be called', (done) => {
+    const mockedResponse = mockDOMResponse({});
+
+    editor({DOM: mockedResponse});
+
+    done();
+  });
+
+  it('renders the editor', (done) => {
+    const mockedResponse = mockDOMResponse({});
+
+    editor({DOM: mockedResponse}).DOM.subscribe(vtree => {
+      assert.equal(vtree.properties.className, 'editor');
+      assert.deepEqual(vtree.children.map(el => el.properties.className), ['actor', 'actor', 'actor']);
+      done();
+    });
   });
 });
